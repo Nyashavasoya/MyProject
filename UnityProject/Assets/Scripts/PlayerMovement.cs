2016b047edc1bad86fb25e71b2d3767c8cc4a93f
Nyashavasoya <nyashavasoya@gmail.com>
2023-06-29 17:35:14 +0530

    Updated PlayerMovement.cs
    Enabled movement of player with keys

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(movementSpeed * HorizontalInput, rb.velocity.y, jumpSpeed * VerticalInput);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+        }
     }
 }
