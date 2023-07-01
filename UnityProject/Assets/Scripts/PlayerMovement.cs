fec1620cc6e8fa18c30e00c8eb16e56c42d8fff1
Nyashavasoya <nyashavasoya@gmail.com>
2023-07-01 16:09:10 +0530

    Update PlayerMovement.cs
    Added Layermask and made a function named is_grounded( ) to check if position of sphere is on the ground or not

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,14 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float movementSpeed = 6f;
     [SerializeField] float jumpSpeed = 5f;
-    // Start is called before the first frame update
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float HorizontalInput = Input.GetAxis("Horizontal");
@@ -25,5 +26,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
         }
+    }
+
+    bool is_grounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
     }
 }
