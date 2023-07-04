37f24598414053ff4ebf0195c2e8a738d8cb5ffd
Nyashavasoya <nyashavasoya@gmail.com>
2023-07-04 07:23:20 +0530

    Added WaypointFollower.cs
    This brings the floor in motion

---
+++ UnityProject/Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointerFollo : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] waypointers;
+    int currentWayPointer = 0;
+
+    [SerializeField] float speed = 1f;
+ 
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, waypointers[currentWayPointer].transform.position) < 0.1f)
+        {
+            currentWayPointer++;
+            if(currentWayPointer >= waypointers.Length)
+            {
+                currentWayPointer = 0;
+            }
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, waypointers[currentWayPointer].transform.position, speed * Time.deltaTime);
+    }
+}
