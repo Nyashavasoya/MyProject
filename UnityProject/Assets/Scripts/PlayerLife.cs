6f3727153ab1670b10e57dd65ebaf5435f8aa840
Nyashavasoya <nyashavasoya@gmail.com>
2023-07-06 02:41:19 +0530

    PlayerLife.cs added

---
+++ UnityProject/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy Body"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<PlayerMovement>().enabled = false;
+        ReloadLevel();
+
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
